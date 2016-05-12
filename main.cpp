a7cfd13900725d4d635c5239a718ea2331fd8478
CarlosMachado <testsubjectno2@hotmail.com>
2016-05-11 19:01:34 -0600

    Empezando

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+#include <ncurses.h>
+
+using std::string;
+using std::ifstream;
+using std::ofstream;
+
+int main(int argc, char*argv[]){
+	initscr();
+	
+	endwin();
+	return 0;
+}
